f5993d953fb23dc2f2334f75f2b5618884a57209
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2019-03-24 09:30:49 +0200

    Initial Commit

---
+++ UIXKit/UIXKit.h
@@ -0,0 +1,19 @@
+//
+//  UIXKit.h
+//  UIXKit
+//
+//  Created by Hugo Prinsloo on 2019/03/24.
+//  Copyright © 2019 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIXKit.
+FOUNDATION_EXPORT double UIXKitVersionNumber;
+
+//! Project version string for UIXKit.
+FOUNDATION_EXPORT const unsigned char UIXKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIXKit/PublicHeader.h>
+
+
